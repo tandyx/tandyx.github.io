5c46f779c441695dbb80e1021d908f4ed99bfad1
Johan Cho <cho.joh@northeastern.edu>
2023-12-21 02:15:58 -0500

    rm json, use css instead for langs

--- src/js/project_init.js
+++ src/js/project_init.js
@@ -1,12 +1,9 @@
 window.addEventListener("load", function () {
   const username = "tandy-c";
-  if (document.querySelectorAll("[data-repo]").length > 0) {
-    readJSON("../src/config/lang_colors.json").then((colorJson) => {
-      document.querySelectorAll("[data-repo]").forEach((el) => {
-        createBar(el.id, username, el.dataset.repo, colorJson);
-      });
-    });
-  }
+
+  document.querySelectorAll("[data-repo]").forEach((el) => {
+    createBar(el.id, username, el.dataset.repo);
+  });
 
   for (const projectId of ["mbta_mapper", "teams_bot", "linprog"]) {
     addProjectEvents(projectId);
@@ -18,10 +15,9 @@ window.addEventListener("load", function () {
  * @param {string} containerId - The id of the element to put the bar chart in
  * @param {string} username - The username of the repo owner
  * @param {string} reponame - The name of the repo
- * @param {object} colorJson - A json object of the colors to use for each language
  * @returns {void}
  */
-function createBar(containerId, username, reponame, colorJson) {
+function createBar(containerId, username, reponame) {
   const container = document.getElementById(containerId);
   const langPromise = getRepoLangs(username, reponame);
   langPromise.then((languages) => {
@@ -37,10 +33,10 @@ function createBar(containerId, username, reponame, colorJson) {
       const bar = document.createElement("div");
       zIndex -= 1;
       totalWidth += languages[lang];
-      bar.className = "bar";
+      bar.classList.add(lang, "bar");
       bar.style.width = `${languages[lang]}%`;
       bar.style.left = `${totalWidth - languages[lang]}%`;
-      bar.style.backgroundColor = colorJson[lang] || "#474747";
+      // bar.style.backgroundColor = colorJson[lang] || "#474747";
       bar.style.zIndex = zIndex;
       bar.dataset.tooltip = `${lang.toLowerCase()} ${languages[lang].toFixed(
         2
@@ -99,17 +95,10 @@ async function getRepoLangs(username, reponame, key = null) {
 }
 
 /**
- * Reads a json file
- * @param {string} file - The path to the json file
- * @returns {Promise} - A promise that resolves to the json object
+ * adds event listeners to the project cards
+ * @param {string} id - The id of the element to get the style of
+ * @returns {string} - The style of the element
  */
-async function readJSON(file) {
-  console.log(file);
-  const ls = await fetch(file);
-  const colorJson = await ls.json();
-  return colorJson;
-}
-
 function addProjectEvents(id) {
   const projectWrapper = document.getElementById(id);
   if (!projectWrapper) return;
